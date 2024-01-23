cceafb8deff48d589c20a3545dd57f533d2eeaa2
Patricia Casenas <pat.casenas@gmail.com>
2024-01-23 21:34:43 +0800

    upskill

---
+++ ice-cream-pos/index.php
@@ -0,0 +1,19 @@
+<?php
+    class User {
+
+    }
+
+    $userOne = new User();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
